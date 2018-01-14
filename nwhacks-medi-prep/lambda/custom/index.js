a077b91beb6610ad1ba1ae70039b153168977253
Kenneth Culala <kaculala@gmail.com>
2018-01-14 02:47:17 -0800

    alexa skill v1. no rds integration

---
+++ nwhacks-medi-prep/lambda/custom/index.js
@@ -0,0 +1,41 @@
+'use strict';
+var Alexa = require("alexa-sdk");
+var APP_ID = "amzn1.ask.skill.6db7c9a9-2b63-419b-9099-e63ea8706571";
+
+exports.handler = function(event, context) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'SymptomLoggerIntent': function () {
+        this.emit('LogSymptom');
+    },
+    'LogSymptom': function () {
+        var part = this.event.request.intent.slots.Part.value;
+        var sensation = this.event.request.intent.slots.Sensation.value;
+        var intensity = this.event.request.intent.slots.Intensity.value;
+        this.attributes['part'] = part;
+        this.attributes['sensation'] = sensation;
+        this.attributes['intensity'] = intensity;
+        console.log("P: " + part + " S: " + sensation + " I: " + intensity);
+        this.response.speak('Your symptom has been recorded successfully. Bye.')
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest' : function() {
+        console.log('Session ended with reason: ' + this.event.request.reason);
+    },
+    'AMAZON.StopIntent' : function() {
+        this.response.speak('Bye. Thanks for using MediPrep');
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent' : function() {
+        this.response.speak('Bye. Thanks for using MediPrep');
+        this.emit(':responseReady');
+    },
+    'Unhandled' : function() {
+        this.response.speak("Sorry, I didn't get that.");
+    }
+};
